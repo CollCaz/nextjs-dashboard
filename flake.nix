831ca1722f1fb038fa6c8e0675e9b1eea8a4683b
CollCaz <yassentombok@proton.me>
2025-02-13 09:41:35 +0200

    flake.nix

---
+++ flake.nix
@@ -0,0 +1,28 @@
+# SPDX-FileCopyrightText: 2021-2025 Akira Komamura
+# SPDX-License-Identifier: Unlicense
+{
+  inputs = {
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    { systems, nixpkgs, ... }:
+    let
+      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+            # Comment out one of these to use an alternative package manager.
+            pnpm
+
+            nodePackages.typescript
+            nodePackages.typescript-language-server
+          ];
+        };
+      });
+    };
+}
